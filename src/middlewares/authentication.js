4de002c7c96f5225a3f748f173574e09e8ebb0c0
TarunRajput3201 <tarunrajput3201@gmail.com>
2022-06-22 13:38:54 +0530

    Tarun

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -3,53 +3,31 @@ const jwt = require("jsonwebtoken");
 const authenticate = function(req, res, next) {
     
        
-//    try{ token = req.headers["x-Api-key"];
-//     if (!token) {token = req.headers["x-api-key"]}
+   try{ token = req.headers["x-Api-key"];
+    if (!token) {token = req.headers["x-api-key"]}
   
    
-//     if (!token) {
+    if (!token) {
 
-//     res.send({ status: false, msg: "token must be present" })}
+    res.send({ status: false, msg: "token must be present" })}
   
     
     
     
-//     let decodedToken = jwt.verify(token, "functionup-radon");
-//     if (!decodedToken)
-//       return res.status(400).send({ status: false, msg: "token is invalid" });
-//   else{
-//     next()}}
-//     catch(err)
-//     {
-//         console.log(err.message)
-//         res.status(500).send(err.message)
-//     }
-
-
-
-try {
-    let token = req.headers["x-Api-key"];
-    if (!token)
-     token = req.headers["x-api-key"];
-    if (!token) return res.status(404).send({ status: false, msg: "token must be present" });
-   console.log(token);
-
-    let UserId=req.params.userId
     let decodedToken = jwt.verify(token, "functionup-radon");
-    let decetUser=decodedToken.userId
-    if (!decodedToken) {
+    if (!decodedToken)
       return res.status(400).send({ status: false, msg: "token is invalid" });
-    }
-    else if(UserId!==decetUser)
-      return res.status(401).send({ status: false, msg: "not allow to modyfy user" });
-    else
-  
-    next()
-  } 
-  catch (err) {
-    console.log("This is the error :", err.message)
-    res.status(500).send({ msg: "Error", error: err.message })
-  }
+  else{
+    next()}}
+    catch(err)
+    {
+        console.log(err.message)
+        res.status(500).send(err.message)
+     }
+
+
+
+
 }
 
 
--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const blog = require("../models/blogModel.js")
 const authorise = function(req, res, next) {
     
     try{token = req.headers["x-Api-key"];
@@ -26,4 +27,29 @@ catch(err)
     res.status(500).send(err.message)
 }
 }
+let authoriseGetAndDelete = async function(req,res,next){
+    let token = req.headers["x-api-key"]
+    let decodedToken = jwt.verify(token, "functionup-radon")
+    let authorId = req.query.authorId
+    let userId = decodedToken.userId
+    if(userId!=authorId){
+      res.status(404).send({status : false, msg : "Not allowed to modify another data"})
+    } 
+    next()
+  }
+   let authorisePutAndDelete = async function(req,res,next){
+    let blogId= req.params.blogId
+  let data = await blog.findById(blogId)
+   let authorId = data.authorId
+    let token = req.headers["x-api-key"]
+    let decodedToken = jwt.verify(token, "functionup-radon")
+    let userId = decodedToken.userId
+     if(authorId!=userId){
+      res.status(403).send({status: false , msg : "Not allowed to modify another data"})
+     }
+     next()
+  
+  }
 module.exports.authorise=authorise
+module.exports.authorisePutAndDelete=authorisePutAndDelete
+module.exports.authoriseGetAndDelete=authoriseGetAndDelete
